d52c502e2e69fa8464887a27ff8c35c92f45fdba
Saraswathi06 <66866596+Saraswathi06@users.noreply.github.com>
2021-11-28 20:23:42 +0530

    Create blynking led.py

---
+++ blynking led.py
@@ -0,0 +1,23 @@
+# import modules
+import RPi.GPIO as GPIO
+import time
+ 
+# setup pins
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8, GPIO.OUT)
+GPIO.setup(12, GPIO.IN)
+ 
+# loop 5 times
+for i in range(5):
+ 
+	# flash output pin 3
+GPIO.output(8, GPIO.HIGH)
+time.sleep(1)
+GPIO.output(8, GPIO.LOW)
+time.sleep(1)
+ 
+	# read input pin 5
+	if GPIO.input(12) == GPIO.HIGH:
+    	print("Pin 12 is on")
+	else:
+    	print("Pin 12 is off")
